f63d95d2b9e87ee5cc9b64ac2e578def02c187eb
paco7828 <koszegipatrik888@gmail.com>
2024-03-03 18:33:21 +0100

    Inserted the code into index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react"
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function CreateResponse({ content }) {
+    return <h3>Chosen color: {content}</h3>
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Comp() {
+    const [chosenColor, setChosenColor] = useState("red");
+    const [submitted, setSubmitted] = useState(false);
+    const handleChange = (e) => {
+        setChosenColor(e.target.value);
+        setSubmitted(false);
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setSubmitted(true);
+    }
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <select value={chosenColor} onChange={handleChange}>
+                    <option value="red">red</option>
+                    <option value="green">green</option>
+                    <option value="blue">blue</option>
+                </select>
+                <input type="submit" />
+            </form>
+            {submitted && <CreateResponse content={chosenColor} />}
+        </>
+    );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Comp />)
